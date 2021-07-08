9077cd612f989781af7ce8e03680544fdc85a1ce
yugmani <yugmani@yahoo.com>
2021-07-07 19:14:29 -0700

    what is new Date()

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('hello!')
+// ***********************************
+// How to Format Dates in JS
+// ***********************************
+
+// The most used method to get the date in JavaScript is the new Date() object.
+
+// By default, when you run new Date() in your terminal, it uses your browser's time zone and displays the date as a full text string, like Wed Jul 07 2021 19:12:32 GMT-0700 (Pacific Daylight Time).
+
+let newdate = new Date();
+// console.log(newdate);
+// Wed Jul 07 2021 19:12:32 GMT-0700 (Pacific Daylight Time)
+
+// formatting date;
+let today = new Date().toLocaleDateString('en-us', {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'short',
+  day: 'numeric'
+});
+
+// console.log(today); // Wednesday, Jul 7, 2021
